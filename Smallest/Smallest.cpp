55ce70e4adc3228dc6915ef95c99e10843594c2c
John Flynn <johnbishopflynn@gmail.com>
2023-06-04 23:43:35 -0400

    added intentional erros for demo

--- Smallest/Smallest.cpp
+++ Smallest/Smallest.cpp
@@ -2,3 +2,16 @@ int main()
 {
     return 0;
 }
+
+// Add a missing simicolon to cause an error
+// int main()
+// {
+//     return 0
+// }
+
+//Createing syntax error to cause error
+// Remember to start with first error and recompile project from there down so it could possibly fix further errors.
+// int main()
+// {
+//     retrn 0;
+// }
