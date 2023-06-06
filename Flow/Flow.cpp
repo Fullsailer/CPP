bd0dc0b435cc48099f92ff618c70cbfce3fc4d48
John Flynn <johnbishopflynn@gmail.com>
2023-06-06 14:01:38 -0400

    added a while loop that quites the program and a for loop for an example.

--- Flow/Flow.cpp
+++ Flow/Flow.cpp
@@ -7,23 +7,43 @@ int main()
     //Declared variables
     int firstNumber;
     int secondNumber;
-    //Write to screen for inputs
-    cout <<"Enter a Number: ";
-    cin >> firstNumber;
-    cout << "You entered " <<firstNumber << ". Enter another number: ";
-    cin >> secondNumber;
+    bool keepgoing = true;
+    int answer;
 
-    //Check if first number is greater than second number.
-    if (firstNumber < secondNumber)
+    while(keepgoing)
     {
-        cout << "The first number, " << firstNumber << ", is less than the second number, " << secondNumber << '\n';
-    }
-    if (firstNumber == secondNumber)
-    {
-        cout << "The first number, " << firstNumber << ", is equal to the second number, " << secondNumber << '\n';
-    }
-    if (firstNumber > secondNumber)
-    {
-        cout << "The first number, " << firstNumber << ", is greater than the second number, " << secondNumber << '\n';
+        //Write to screen for inputs
+        cout << "Enter a Number: ";
+        cin >> firstNumber;
+        cout << "You entered " << firstNumber << ". Enter another number: ";
+        cin >> secondNumber;
+
+        //Check if first number is greater than second number.
+        if (firstNumber < secondNumber)
+        {
+            cout << "The first number, " << firstNumber << ", is less than the second number, " << secondNumber << '\n';
+        }
+        else
+        {
+            cout << "The first number, " << firstNumber << ", is equal to the second number, " << secondNumber << '\n';
+        }
+        if (firstNumber == secondNumber)
+        {
+            cout << "The first number, " << firstNumber << ", is equal to the second number, " << secondNumber << '\n';
+        }
+        if (firstNumber > secondNumber)
+        {
+            cout << "The first number, " << firstNumber << ", is greater than the second number, " << secondNumber << '\n';
+        }
+        for (int loop = 0; loop < 10; loop++)
+        {
+            cout << loop << " ";
+        }
+        cout <<  "Compare another pair? 0 means no, any other number means yrs: ";
+        cin >> answer;
+        if (answer == 0)
+        {
+            keepgoing = false;
+        }        
     }
 }
